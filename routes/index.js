e8cf765d76eab7c450e6ae7d229b052cee8fb55c
kalvyn-lu <kalvynlu@gmail.com>
2015-11-17 12:25:03 -0700

    Changed the background

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,15 @@ var express = require('express');
 var pokeapi = require('pokenode');
 var router = express.Router();
 
-pokeapi.pokemon(1, function(err, data) {
-    if(err) {
-        // handle err
-    } else {
-        console.log(data);
-    }
-});
+// pokeapi.pokemon(1, function(err, data) {
+//     if(err) {
+//         // handle err
+//     } else {
+//         console.log(data);
+//     }
+// });
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
