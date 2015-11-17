37094dc8a92499061367cda4ddf7c7cc12d0f154
Todd Beckman <toddbeckman1@gmail.com>
2015-11-17 15:18:28 -0700

    stuff

--- routes/teamview.js
+++ routes/teamview.js
@@ -13,7 +13,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var data = [bulbasaur, bulbasaur];
-  res.render('teamview', { title: 'Express' });
+  res.render('teamview', { title: 'Express', data: data });
 });
 
 module.exports = router;
